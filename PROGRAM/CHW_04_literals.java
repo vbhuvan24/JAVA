13e93744ab4714412379c7a758e3d2ac986d7a57
Yash Verma <56463836+vbhuvan24@users.noreply.github.com>
2021-12-08 21:31:26 +0530

    Add files via upload

---
+++ PROGRAM/CHW_04_literals.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class CHW_04_literals {
+    public static void main(String[] args) {
+        byte age = 24;
+        String a = "hello";
+        System.out.println(age);
+        System.out.println(a);
+    }
+}
